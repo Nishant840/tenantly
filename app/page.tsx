83efc8b94e5aeb1db2dde14e7f5feabf02ddc580
Nishant840 <nishant7896545@gmail.com>
2026-02-03 03:26:27 +0530

    implemented redirect logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma"
+import authOptions from "@/lib/auth-options";
 
-export default function Home() {
-  return (
-    <div>
-        hii there
-    </div>
-  );
+
+export default async function Home(){
+    const session = await getServerSession(authOptions);
+
+    if(!session?.user?.email){
+        redirect("/api/auth/signin");
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {email: session.user.email},
+        include: {orgMemberships:true}
+    });
+
+    if(!user || user.orgMemberships.length == 0){
+        redirect("/onboarding")
+    }
+
+    redirect("/dashboard")
 }
---
+++ lib/auth-options.ts
@@ -0,0 +1,29 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+import {prisma} from "@/lib/prisma"
+
+const authOptions: AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    callbacks: {
+        async signIn({ user }){
+            if(!user.email) return false;
+
+            await prisma.user.upsert({
+                where : {email: user.email},
+                update: {},
+                create: {
+                    email: user.email,
+                    name: user.name
+                },
+            });
+
+            return true;
+        }
+    }
+}
+export default authOptions
