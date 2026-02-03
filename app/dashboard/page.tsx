387119e66a6544e3417a28d98b77c84d3b21cda9
Nishant840 <nishant7896545@gmail.com>
2026-02-04 04:13:23 +0530

    making dashboard page to show organization minimal UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,41 @@
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { prisma } from "@/lib/prisma"
+import authOptions from "@/lib/auth-options"
 
-export default function DashboardPage(){
+export default async function DashboardPage(){
+    const session = await getServerSession(authOptions);
+
+    if(!session?.user?.email){
+        redirect("/api/auth/signin")
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {email: session.user.email},
+        include: {
+            orgMemberships: {
+                include: {
+                    organization: true
+                },
+            },
+        },
+    });
+
+    if(!user || user.orgMemberships.length == 0){
+        redirect("/onboarding");
+    }
+
+    const activeOrg = user.orgMemberships[0].organization;
     return <div>
         <h1>Dashboard</h1>
+        <p>
+            <strong>Organization:</strong>{activeOrg.name}
+        </p>
+        <p>
+            <strong>Plan:</strong> {activeOrg.plan}
+        </p>
+        <p>
+            <strong>Slug:</strong> {activeOrg.slug}
+        </p>
     </div>
 }
